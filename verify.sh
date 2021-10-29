f06cf4d26c0d2dfd48918d0558e065b9d8f69e33
Stefan Baebler <stefan.baebler@gmail.com>
2021-10-29 09:34:14 +0200

    Generate newlines manually added in #3

--- verify.sh
+++ verify.sh
@@ -4,9 +4,11 @@ export TZ=UTC
 echo "# Results"
 
 for i in samples/*.txt; do
+  echo
   echo "## $i:"
   echo
   echo "![Certificate $i](${i/.txt/.png})"
+  echo
   echo '```plain'
   corona-decoder -v -f "$i"
   echo '```'
